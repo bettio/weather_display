004a3d71246169972c14518a92aa24d6cf1ea6fd
Davide Bettio <davide@uninstall.it>
2022-05-18 00:24:14 +0200

    Make it an AtomVM Elixir project

---
+++ lib/main.ex
@@ -0,0 +1,5 @@
+defmodule Main do
+  def start() do
+    :erlang.display("Started.")
+  end
+end
--- mix.exs
+++ mix.exs
@@ -5,9 +5,13 @@ defmodule WeatherDisplay.MixProject do
     [
       app: :weather_display,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      atomvm: [
+        start: Main,
+        flash_offset: 0x210000
+      ]
     ]
   end
 
@@ -21,8 +25,8 @@ defmodule WeatherDisplay.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:exatomvm, github: "AtomVM/exatomvm", runtime: false},
+      {:avm_scene, github: "AtomVM/avm_scene"}
     ]
   end
 end
